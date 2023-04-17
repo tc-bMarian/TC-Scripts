2a253841dcd606a1b6a54472e2d1da2c62c010e1
tc-bMarian <128073125+tc-bMarian@users.noreply.github.com>
2023-04-17 13:29:13 -0700

    Create No Alert.user.js

---
+++ No Alert.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         No Alert
+// @namespace    https://livec17a05.adaptiveplanning.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://livec17a05.adaptiveplanning.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// @run-at      document-start
+// ==/UserScript==
+
+// removes alert :D
+// just be careful, it won't warn you to save changes.
+top.window.onbeforeunload = null;
+
+// SB seems to hold data & formulas on user. SB.setTimer is called for part of the heartbeat.
+
+
+
+(function() {
+    'use strict';
+
+
+})();
